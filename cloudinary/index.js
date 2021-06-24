44fb91c9e420520b126252905201a32c2842e345
Rahul <rahuljadav2121@gmail.com>
2021-06-24 19:17:36 +0530

    Ready to push

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,10 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
+const dotenv = require('dotenv')
+dotenv.config({path: './config/config.env'})
 cloudinary.config({
-    cloud_name: 'dceofbe10',
-    api_key:'943639264385154',
-    api_secret:'pL7s1CJetdZuRtWIt_3dgyZExPc'
+    cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+    api_key:`${process.env.CLOUDINARY_KEY}`,
+    api_secret:`${process.env.CLOUDINARY_SECRET}`
 });
  
 const storage = new CloudinaryStorage({
